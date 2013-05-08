b383e4a062612bf08af57fd0fd9420978a6d39b4
Birger Schmidt <bs-github@netgaroo.com>
2013-05-08 13:47:13 +1000

    add options to configure elasticsearch disk usage

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,20 +2,22 @@
 #
 class graylog2server
 (
-  $version               = 'present',
-  $enable                = true,
-  $start                 = true,
-  $syslog_listen_address = '0.0.0.0',
-  $syslog_listen_port    = '514',
-  $syslog_enable_udp     = true,
-  $syslog_enable_tcp     = false,
-  $es_cluster_name       = 'graylog2',
-  $mongodb_useauth       = false,
-  $mongodb_user          = 'graylog2user',
-  $mongodb_password      = 'redacted',
-  $mongodb_host          = '127.0.0.1',
-  $mongodb_database      = 'graylog2',
-  $mongodb_port          = '27017',
+  $version                             = 'present',
+  $enable                              = true,
+  $start                               = true,
+  $syslog_listen_address               = '0.0.0.0',
+  $syslog_listen_port                  = '514',
+  $syslog_enable_udp                   = true,
+  $syslog_enable_tcp                   = false,
+  $es_cluster_name                     = 'graylog2',
+  $mongodb_useauth                     = false,
+  $mongodb_user                        = 'graylog2user',
+  $mongodb_password                    = 'redacted',
+  $mongodb_host                        = '127.0.0.1',
+  $mongodb_database                    = 'graylog2',
+  $mongodb_port                        = '27017',
+  $elasticsearch_max_docs_per_index    = '20000000',
+  $elasticsearch_max_number_of_indices = '20',
 )
 {
   class{'graylog2server::install': } ->
